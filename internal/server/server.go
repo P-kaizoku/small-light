6b3c12a0717fc30050a604168a8f8b1c9a150af1
pabitra <pabitram322@gmail.com>
2026-08-04 16:32:09 +0530

    feat: implement server and bind health func to /health route

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,3 +1,33 @@
 package server
 
-func New(cfg)
+import (
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/P-kaizoku/small-light/internal/config"
+	"github.com/P-kaizoku/small-light/internal/handler"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
+)
+
+func New(cfg config.Config, logger *slog.Logger, pool *pgxpool.Pool, rdb *redis.Client) *http.Server {
+
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+
+	health := handler.NewHealth(pool, rdb, logger)
+
+	r.Get("/health", health.Check)
+
+	return &http.Server{
+		Addr:              ":" + cfg.Port,
+		Handler:           r,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+}
